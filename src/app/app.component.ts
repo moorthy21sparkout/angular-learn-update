0d4d0689b5f1daad8bffd91ad915e49f470b241a
moorthy21sparkout <moorthy@sparkouttech.com>
2024-12-12 10:57:38 +0530

    linked-signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, linkedSignal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,32 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'linkedsignal';
+
+  sourceSignal = signal(0);
+
+  // computed
+  updateSignal3 = computed(() => this.sourceSignal() * 2);
+
+
+  // short hand
+  updateSignal = linkedSignal(() => this.sourceSignal() * 5);
+
+
+  //source and computation
+  updateSignal1 = linkedSignal({
+    source: this.sourceSignal,
+    computation: () => this.sourceSignal() * 5
+  });
+
+  updateSignal2 = linkedSignal({
+    source: this.updateSignal1,
+    computation: () => this.sourceSignal() * 5
+  });
+  updatedsignal(){
+    this.sourceSignal.set(5);
+    this.updateSignal.set(5);
+
+    // this is error because updateSignal is computed only for read
+    // this.updateSignal.set(3)    
+  }
 }
